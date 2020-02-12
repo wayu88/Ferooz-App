73425197b490de3b60bee12c81f3d665e837720c
wayu88 <wayukumar@gmail.com>
2020-02-13 02:26:46 +0530

    poc completed
    poc completed

--- src/component/App.js
+++ src/component/App.js
@@ -1,6 +1,8 @@
 import React,{PropTypes} from 'react';
 import Header from './Header';
 import ComparisionComponent from './Comparision'
+import InputComponent from './InputComponent';
+import LightComponent from './LightComponent';
 import PageHeader from './PageHeader';
 
 export class App extends React.Component{
@@ -85,28 +87,75 @@ export class App extends React.Component{
                         "eQFF":"32",
                         "eQSL":""
                     }
-                ]
+                ],
+                selectedHeader:""
             }
+
+            this.onHeaderClick = this.onHeaderClick.bind(this);
         }
 
+        onHeaderClick(param) {
+            //console.log("inside App :: ",param);
+            
+            this.setState({selectedHeader:param.name});
+        }
         
         render(){
-                    return(
+
+                    console.log(this.state.selectedHeader)
+
+                return(
                             <div className="container-fluid">
                                 <div className="row">
-                                    <PageHeader/>
+                                    <PageHeader onHeaderClick={this.onHeaderClick}/>
                                 </div>
-                                
-                                <div className="well">
-                                    <div className="row">
-                                        <Header title="Total Dark" dataArray={this.state.dataArray}/>
-                                        <Header title="Total Light" dataArray={this.state.dataArray}/>
-                                        <Header title="Total Difference" dataArray={this.state.dataArray}/>
-                                        <Header title="Percent Difference" dataArray={this.state.dataArray}/>
-                                        <Header title="Count of loc diff's" dataArray={this.state.dataArray}/>
-                                        <ComparisionComponent headerArray={this.state.comparisionHeader} dataArray={this.state.comaparisionData}/>
+                                {
+                                    this.state.selectedHeader === 'Input Page' ? 
+                                        (<InputComponent title="input"/> ) : 
+
+                                    this.state.selectedHeader === 'Rated-Comparision' ?
+                                      (
+                                      
+                                        <div className="row" style={{marginRight:'0px',marginLeft:'0px'}}>
+                                            <div className="col-sm-12" style={{marginTop:'30px'}}>
+                                                 <div className="panel panel-default" style={{textAlign:'center'}}><h3>Rated-Comparision Page</h3></div>
+                                            </div>
+                                            <div className="well" style={{marginTop:'100px'}}>
+                                                    <div className="row">
+                                                        <Header title="Total Dark" dataArray={this.state.dataArray}/>
+                                                        <Header title="Total Light" dataArray={this.state.dataArray}/>
+                                                        <Header title="Total Difference" dataArray={this.state.dataArray}/>
+                                                        <Header title="Percent Difference" dataArray={this.state.dataArray}/>
+                                                        <Header title="Count of loc diff's" dataArray={this.state.dataArray}/>
+                                                        <ComparisionComponent headerArray={this.state.comparisionHeader} dataArray={this.state.comaparisionData}/>
+                                                    </div>
+                                                </div> 
+                                         </div>       
+                                        ) :
+                                      
+                                    this.state.selectedHeader === 'Dark'?
+                                      (<InputComponent title="Dark Page"/>)  : this.state.selectedHeader === 'Modeled-Comparision'?
+                              (
+                                <div className="row" style={{marginRight:'0px',marginLeft:'0px'}}>
+                                    <div className="col-sm-12" style={{marginTop:'30px'}}>
+                                        <div className="panel panel-default" style={{textAlign:'center'}}><h3>Modeled-Comparision Page</h3></div>
                                     </div>
-                                </div>  
+                                    <div className="well" style={{marginTop:'100px'}}>
+                                            <div className="row">
+                                                <Header title="Total Dark" dataArray={this.state.dataArray}/>
+                                                <Header title="Total Light" dataArray={this.state.dataArray}/>
+                                                <Header title="Total Difference" dataArray={this.state.dataArray}/>
+                                                <Header title="Percent Difference" dataArray={this.state.dataArray}/>
+                                                <Header title="Count of loc diff's" dataArray={this.state.dataArray}/>
+                                                <ComparisionComponent headerArray={this.state.comparisionHeader} dataArray={this.state.comaparisionData}/>
+                                            </div>
+                                    </div>
+                                </div>    
+                          ):(<LightComponent title="Light Page"/>)
+
+                                    
+                                }
+                               
                                
                             </div>
                     );
--- src/component/PageHeader.js
+++ src/component/PageHeader.js
@@ -6,10 +6,44 @@ export default class PageHeader extends React.Component{
 
     constructor(props){
         super(props);
+        this.state = {
+            links: [
+                {
+                    id: 0,
+                    name: "Input"
+                },
+                {
+                    id: 1,
+                    name: "Rated-Comparision"
+                },
+                {
+                    id: 2,
+                    name: "Modeled-Comparision"
+                },
+                {
+                    id: 3,
+                    name: "Dark"
+                },
+                {
+                    id: 4,
+                    name: "Light"
+                }
+            ],
+            activeLink: 0
+        }
     }
+
+    handleClick(id){
+        this.setState({ activeLink: id });
+        let linkTemp = this.state.links[id];
+        this.props.onHeaderClick(linkTemp);
+    };
     
     render(){
+        const { links, activeLink } = this.state;
+       
         return(
+            
             <div className="container-sub-nav">
                     <div className="row" style={{marginLeft:'0px',marginRight:'0px'}}>
                         <nav className="navbar navbar-expand-lg">
@@ -20,24 +54,13 @@ export default class PageHeader extends React.Component{
                                     </div>
 
                                     <div className="col-sm-10">
-                                    <ul className="navbar-nav" style={{listStyleType:'none'}}>
-                                            <li className="nav-item active">
-                                                <a className="nav-link" href="/blog">Input<span className="sr-only">(current)</span></a>
-                                            </li>
-                                            <li className="nav-item">
-                                                <a className="nav-link">Rated-Comparision</a>
-                                            </li>
-                                            <li className="nav-item">
-                                                <a className="nav-link">Modeled-Comparision</a>
-                                            </li>
-                                            <li className="nav-item">
-                                                <a className="nav-link">Dark</a>
-                                            </li>
-                                            <li className="nav-item">
-                                                <a className="nav-link">Light</a>
-                                            </li>
-                                                        
-                                    </ul>
+                                        <ul className="navbar-nav" style={{listStyleType:'none'}}>
+                                            {this.state.links.map((item, idx) => (
+                                                <li key={idx} className={item.id === activeLink ? 'nav-item active' : 'nav-item'} onClick={() => this.handleClick(item.id)}>
+                                                    <a className="nav-link">{item.name}</a>
+                                                </li>   
+                                            ))}  
+                                        </ul>
                                     </div>
                                 </div>
                                
